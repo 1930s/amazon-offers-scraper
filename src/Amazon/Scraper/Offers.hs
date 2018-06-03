608a303c8345f686ab7e6cd5418d1ca81641d457
Robert K <kornacki@live.com>
2018-06-03 17:04:50 -0400

    Reimplemented library using Reader in addition to also cleaning up Types to make all functions more readable and clear. Opted to not use ReaderT since I have no need for the transformer.

--- src/Amazon/Scraper/Offers.hs
+++ src/Amazon/Scraper/Offers.hs
@@ -2,24 +2,25 @@
 
 module Amazon.Scraper.Offers where
 
+import           Control.Monad.Reader        (Reader, runReader, ask, liftIO)
 import qualified Data.ByteString.Lazy        as BSL
-import           Data.ByteString.Lazy.Char8  (pack, unpack, dropWhile)
 import qualified Data.ByteString.Lazy.Char8  as BSLC
+import           Data.ByteString.Lazy.Char8  (pack, unpack, dropWhile)
 import           Extra.Util.Func             (cleanupText, extractCurrency)
 import           Data.Maybe                  (fromMaybe)
-import           Text.HTML.Scalpel.Core
-import           Text.Read
+import           Text.HTML.Scalpel.Core      ((@:), (@=), scrapeStringLike, text, attr, html, htmls)
 
-type Webpage = BSL.ByteString
+type PageHTML = BSL.ByteString
+type OfferHTML = BSL.ByteString
 
--- | Contains all scraped data from the Amazon Offers Webpage.
+-- | Contains all scraped data from the Amazon Offers PageHTML.
 data AmazonProduct = AmazonProduct { title :: Maybe BSL.ByteString,
                                      image :: Maybe BSL.ByteString,
                                      azASIN :: Maybe BSL.ByteString,
                                      offers :: [AmazonOffer]
                                    } deriving (Show)
 
--- | Contains all scraped data of a single offer from the Amazon Offers Webpage.
+-- | Contains all scraped data of a single offer from the Amazon Offers PageHTML.
 data AmazonOffer = AmazonOffer { price :: Maybe Float,
                                  shipping :: Maybe Float,
                                  totalPrice :: Maybe Float,
@@ -31,76 +32,90 @@ data AmazonOffer = AmazonOffer { price :: Maybe Float,
 
 
 -- | Creates an 'AmazonProduct' based off Amazon Offers webpage html.
-createAmazonProduct :: Webpage -> AmazonProduct
-createAmazonProduct webpage = AmazonProduct { title = scrapeTitle webpage,
-                                              image = scrapeImage webpage,
-                                              azASIN = scrapeASIN webpage,
-                                              offers = scrapeOffers webpage
-                                              }
+-- createAmazonProduct :: PageHTML -> AmazonProduct
+createAmazonProduct webpage = runReader createProd webpage
+  where createProd = do
+          title <- scrapeTitle
+          img <- scrapeImage
+          azASIN <- scrapeASIN
+          offers <- scrapeOffers
+          return AmazonProduct {  title = title,
+                                  image = img,
+                                  azASIN = azASIN,
+                                  offers = offers
+                                }
+
 -- | Scrapes product title from webpage.
-scrapeTitle :: Webpage -> Maybe (BSL.ByteString)
-scrapeTitle webpage = fmap cleanupText $ scrapeStringLike webpage (text titleSelector)
+scrapeTitle :: Reader PageHTML (Maybe BSL.ByteString)
+scrapeTitle = ask >>= return . fmap cleanupText . flip scrapeStringLike (text titleSelector)
   where titleSelector = "h1" @: ["role" @= "main"]
 
 -- | Scrapes image from webpage.
-scrapeImage :: Webpage -> Maybe (BSL.ByteString)
-scrapeImage webpage = scrapeStringLike webpage (attr "src" imageSelector)
+scrapeImage :: Reader PageHTML (Maybe BSL.ByteString)
+scrapeImage = ask >>= return . flip scrapeStringLike (attr "src" imageSelector)
   where imageSelector = "img" @: ["alt" @= "Return to product information"]
 
 -- | Scrapes Amazon ASIN from webpage.
-scrapeASIN :: Webpage -> Maybe (BSL.ByteString)
-scrapeASIN webpage = fmap getASIN $ scrapeStringLike webpage (html asinSelector)
+scrapeASIN :: Reader PageHTML (Maybe BSL.ByteString)
+scrapeASIN = ask >>= return . fmap getASIN . flip scrapeStringLike (html asinSelector)
   where asinSelector = "div" @: ["id" @= "olpProductImage"]
         getASIN = BSLC.takeWhile (/= '/') . BSLC.dropWhile (/= 'B')
 
-
--- | Creates an 'AmazonOffer' based off of the offer html.
-createAmazonOffer :: BSL.ByteString -> AmazonOffer
-createAmazonOffer offHtml = AmazonOffer { price = offPrice,
-                                          shipping = offShipping,
-                                          totalPrice = (+) <$> offPrice <*> offShipping,
-                                          condition = scrapeOfferCondition offHtml,
-                                          description = scrapeOfferDescription offHtml,
-                                          sellerName = scrapeOfferSellerName offHtml,
-                                          sellerRating = scrapeOfferSellerRating offHtml}
-  where offPrice = scrapeOfferPrice offHtml
-        offShipping = scrapeOfferShipping offHtml
-
-
 -- | Scrapes all 'AmazonOffer's from webpage.
-scrapeOffers :: Webpage -> [AmazonOffer]
-scrapeOffers webpage = fmap createAmazonOffer $ fromMaybe [] $ scrapeStringLike webpage (htmls offersSelector)
+scrapeOffers :: Reader PageHTML [AmazonOffer]
+scrapeOffers = ask >>= return . fmap (runReader createAmazonOffer) . fromMaybe [] . flip scrapeStringLike (htmls offersSelector)
   where offersSelector = "div" @: ["class" @= "a-row a-spacing-mini olpOffer"]
 
+
+-- | Creates an 'AmazonOffer' based off of the offer html.
+createAmazonOffer :: Reader PageHTML AmazonOffer
+createAmazonOffer = ask >>= return . runReader createOffer
+  where createOffer = do
+          shipping <- scrapeOfferShipping
+          price <- scrapeOfferPrice
+          condition <- scrapeOfferCondition
+          description <- scrapeOfferDescription
+          sellerName <- scrapeOfferSellerName
+          sellerRating <- scrapeOfferSellerRating
+          return sellerRating
+          return $ AmazonOffer {  price = price,
+                                  shipping = shipping,
+                                  totalPrice = (+) <$> price <*> shipping,
+                                  condition = condition,
+                                  description = description,
+                                  sellerName = sellerName,
+                                  sellerRating = sellerRating }
+
+
 -- | Scrape offer price from offer html.
-scrapeOfferPrice :: BSL.ByteString -> Maybe Float
-scrapeOfferPrice offHtml = extractCurrency $ fromMaybe "" $ scrapeStringLike offHtml (text priceSelector)
+scrapeOfferPrice :: Reader OfferHTML (Maybe Float)
+scrapeOfferPrice = ask >>= return . extractCurrency . fromMaybe "" . flip scrapeStringLike (text priceSelector)
   where priceSelector = "span" @: ["class" @= "a-size-large a-color-price olpOfferPrice a-text-bold"]
 
 -- | Scrape offer shipping from offer html.
-scrapeOfferShipping :: BSL.ByteString -> Maybe Float
-scrapeOfferShipping offHtml = extractShipping $ fromMaybe " " $ scrapeStringLike offHtml (text shippingSelector)
+scrapeOfferShipping :: Reader OfferHTML (Maybe Float)
+scrapeOfferShipping = ask >>= return . extractShipping . fromMaybe " " . flip scrapeStringLike (text shippingSelector)
   where shippingSelector = "p" @: ["class" @= "olpShippingInfo"]
         extractShipping s = if (BSLC.elem 'F' s) then Just 0.00 else extractCurrency s
 
 -- | Scrape offer condition from offer html.
-scrapeOfferCondition :: BSL.ByteString -> Maybe BSL.ByteString
-scrapeOfferCondition offHtml = fmap cleanUsedConditionText $ scrapeStringLike offHtml (text conditionSelector)
+scrapeOfferCondition :: Reader OfferHTML (Maybe BSL.ByteString)
+scrapeOfferCondition = ask >>= return . fmap cleanUsedConditionText . flip scrapeStringLike (text conditionSelector)
   where conditionSelector = "span" @: ["class" @= "a-size-medium olpCondition a-text-bold"]
         cleanUsedConditionText = cleanupText . BSLC.drop 2 . BSLC.dropWhile (/= '-')
 
 -- | Scrape offer description from offer html.
-scrapeOfferDescription :: BSL.ByteString -> Maybe BSL.ByteString
-scrapeOfferDescription offHtml = fmap cleanupText $ scrapeStringLike offHtml (text descriptionSelector)
+scrapeOfferDescription :: Reader OfferHTML (Maybe BSL.ByteString)
+scrapeOfferDescription = ask >>= return . fmap cleanupText . flip scrapeStringLike (text descriptionSelector)
   where descriptionSelector = "div" @: ["class" @= "expandedNote"]
 
 -- | Scrape offer seller name from offer html.
-scrapeOfferSellerName :: BSL.ByteString -> Maybe BSL.ByteString
-scrapeOfferSellerName offHtml = fmap cleanupText $ scrapeStringLike offHtml (text sellerNameSelector)
+scrapeOfferSellerName :: Reader OfferHTML (Maybe BSL.ByteString)
+scrapeOfferSellerName = ask >>= return . fmap cleanupText . flip scrapeStringLike (text sellerNameSelector)
   where sellerNameSelector = "h3" @: ["class" @= "a-spacing-none olpSellerName"]
 
 -- | Scrape offer seller rating from offer html.
-scrapeOfferSellerRating :: BSL.ByteString -> Maybe BSL.ByteString
-scrapeOfferSellerRating offHtml = fmap cleanupRating $ scrapeStringLike offHtml (text sellerRatingSelector)
+scrapeOfferSellerRating :: Reader OfferHTML (Maybe BSL.ByteString)
+scrapeOfferSellerRating = ask >>= return . fmap cleanupRating . flip scrapeStringLike (text sellerRatingSelector)
   where sellerRatingSelector = "p" @: ["class" @= "a-spacing-small"]
         cleanupRating = cleanupText . BSLC.drop 45
